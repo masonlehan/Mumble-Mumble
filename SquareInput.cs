6d57a60d624a1c3dcbc5cfe2bf81f98ce566b9d6
masonlehan <masonlehan@icloud.com>
2026-09-13 16:23:49 -0500

    Add files via upload

---
+++ SquareInput.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SquareInput : MonoBehaviour
+{
+    public bool coins;
+    public int health; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+       void OnMove()
+    {
+        Debug.Log("Input");
+    }
+}
